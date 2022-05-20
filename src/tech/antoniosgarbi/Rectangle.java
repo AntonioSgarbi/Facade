21af72ecd10555fe4c783cd9201425da5d78f91c
antoniosgarbi <antonio.sgarbi@hotmail.com>
2022-05-20 02:10:22 -0300

    Commit from script

---
+++ src/tech/antoniosgarbi/Rectangle.java
@@ -0,0 +1,8 @@
+package tech.antoniosgarbi;
+
+public class Rectangle implements Shape {
+    @Override
+    public void draw() {
+        System.out.println("Shape: rectangle");
+    }
+}
---
+++ src/tech/antoniosgarbi/main/Main.java
@@ -0,0 +1,14 @@
+package tech.antoniosgarbi.main;
+
+import tech.antoniosgarbi.ShapeMaker;
+
+public class Main {
+    public static void main(String[] args) {
+        ShapeMaker shapeMaker = new ShapeMaker();
+
+        shapeMaker.drawCircle();
+        shapeMaker.drawRectangle();
+        shapeMaker.drawSquare();
+    }
+
+}
